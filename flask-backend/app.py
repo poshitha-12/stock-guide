4a4eef397930409e896c2a16eba3c8cdc8ed9962
poshitha <poshitha@mayaglobal.io>
2025-03-27 16:51:52 +0530

    docker added

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -29,4 +29,5 @@ def get_data():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
+
